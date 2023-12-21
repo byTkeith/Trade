2b40d796eef02da4cc85a8113b7e7854aff13048
Your Name <you@example.com>
2023-12-21 23:45:58 +0200

    used an array to store file values

--- Trade.java
+++ Trade.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Trade{
     public static void main(String[] args) {
+        ArrayList<Integer> intFile= new ArrayList<>();
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter the Trade sheet:");
         String fileName=scanner.nextLine();
@@ -18,6 +19,7 @@ public class Trade{
                 scanner.close();
             }
         }
+        for(String lines: f)
         
     }
 }
