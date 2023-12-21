1d861cd479d6d5741e37b9394af1405c20192cbe
Your Name <you@example.com>
2023-12-21 23:39:43 +0200

    added scanner as file operator

--- Trade.java
+++ Trade.java
@@ -1,5 +1,23 @@
+import java.util.*;
+import java.io.*;
 public class Trade{
     public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the Trade sheet:");
+        String fileName=scanner.nextLine();
+        try{
+            File file=new File(fileName);
+            scanner=new Scanner(file);
+            while(scanner.hasNextLine()){
+
+            }
+        }catch(FileNotFoundException e){
+            System.out.println("File not found Askies");
+        }finally{
+            if(scanner!=null){
+                scanner.close();
+            }
+        }
         
     }
 }
