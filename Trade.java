8653d0f4cceda2f99bd2639b748297f6e455a0b3
Your Name <you@example.com>
2023-12-21 23:51:10 +0200

    used a for loop for the sheet

--- Trade.java
+++ Trade.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 public class Trade{
     public static void main(String[] args) {
-        ArrayList<Integer> intFile= new ArrayList<>();
+        ArrayList<String> intFile= new ArrayList<>();
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter the Trade sheet:");
         String fileName=scanner.nextLine();
@@ -10,6 +10,7 @@ public class Trade{
             File file=new File(fileName);
             scanner=new Scanner(file);
             while(scanner.hasNextLine()){
+                intFile.add(scanner.nextLine());
 
             }
         }catch(FileNotFoundException e){
@@ -19,7 +20,9 @@ public class Trade{
                 scanner.close();
             }
         }
-        for(String lines: f)
+        for(String lines: intFile){
+            System.out.println(lines);
+        }
         
     }
 }
